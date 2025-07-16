e5f97edbc3a045167f424eac44608ee15d71829c
SwethaSenthilkumar <saiswethasalem@gmail.com>
2025-07-16 19:51:50 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,38 @@
+#int
+a=100
+print(a)
+print(type(a))
+
+#float
+a=2+8j
+print(a)
+print(type(a))
+
+#complex
+a=2+3j
+print(a)
+print(type(a))
+
+#bool
+a=True
+print(a)
+print(type(a))
+
+#str
+a="swetha"
+print(a)
+print(type(a))
+
+a="""This
+    is
+    my
+    first 
+    program"""
+print(a)
+
+name="swetha"
+print("my name is"+name)
+
+value=40
+print("my pin value",value)
+
